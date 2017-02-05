3e1010e42b314b9dddd658428329e3b3da6fc8d5
Karol Lewandowski <karol86@gmail.com>
2017-02-05 21:21:04 +0100

    #52: Use API Java objects completion
    - minor cleanups

--- src/main/kotlin/co/nums/intellij/aem/htl/editor/actions/HtlExpressionTypedHandler.kt
+++ src/main/kotlin/co/nums/intellij/aem/htl/editor/actions/HtlExpressionTypedHandler.kt
@@ -10,33 +10,34 @@ import com.intellij.psi.PsiFile
 
 class HtlExpressionTypedHandler : TypedHandlerDelegate() {
 
-    override fun beforeCharTyped(charTyped: Char, project: Project?, editor: Editor, file: PsiFile, fileType: FileType?): TypedHandlerDelegate.Result {
+    override fun beforeCharTyped(charTyped: Char, project: Project?, editor: Editor, file: PsiFile, fileType: FileType?): Result {
         if (charTyped == '}' && file.isHtl()) {
             val offset = editor.caretModel.offset
             if (offset < 2 || offset >= editor.document.textLength) {
-                return TypedHandlerDelegate.Result.CONTINUE
+                return Result.CONTINUE
             }
             val nextChar = editor.document.charsSequence[offset]
             if (nextChar == '}') {
                 editor.moveCaret(1)
-                return TypedHandlerDelegate.Result.STOP
+                return Result.STOP
             }
         }
-        return TypedHandlerDelegate.Result.CONTINUE
+        return Result.CONTINUE
     }
 
-    override fun charTyped(charTyped: Char, project: Project?, editor: Editor, file: PsiFile): TypedHandlerDelegate.Result {
+    override fun charTyped(charTyped: Char, project: Project?, editor: Editor, file: PsiFile): Result {
         if (charTyped == '{' && file.isHtl()) {
             val offset = editor.caretModel.offset
             if (offset < 2 || offset > editor.document.textLength) {
-                return TypedHandlerDelegate.Result.CONTINUE
+
+                return Result.CONTINUE
             }
             val previousChar = editor.document.charsSequence[offset - 2]
             if (previousChar == '$') {
                 editor.document.insertString(offset, "}")
             }
         }
-        return TypedHandlerDelegate.Result.CONTINUE
+        return Result.CONTINUE
     }
 
 }
