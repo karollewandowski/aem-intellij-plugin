0479c47c9a9f7883358e781a70349aa58e9cb96e
Karol Lewandowski <karol86@gmail.com>
2017-11-05 11:29:50 +0100

    #86: Make jcr_root directory configurable
    - unit tests for language substitutor

--- src/main/kotlin/co/nums/intellij/aem/htl/file/HtlLanguageSubstitutor.kt
+++ src/main/kotlin/co/nums/intellij/aem/htl/file/HtlLanguageSubstitutor.kt
@@ -4,21 +4,19 @@ import co.nums.intellij.aem.htl.HtlLanguage
 import co.nums.intellij.aem.service.jcrRoots
 import co.nums.intellij.aem.settings.aemSettings
 import com.intellij.ide.highlighter.HtmlFileType
+import com.intellij.lang.Language
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.LanguageSubstitutor
 
 class HtlLanguageSubstitutor : LanguageSubstitutor() {
 
-    override fun getLanguage(file: VirtualFile, project: Project) =
+    override fun getLanguage(file: VirtualFile, project: Project): Language? =
             if (project.aemSettings.aemSupportEnabled && file.isHtlFile(project)) HtlLanguage
             else null
 
-    private fun VirtualFile.isHtlFile(project: Project) = this.isHtml() && this.isInJcrRootDirectory(project)
+    private fun VirtualFile.isHtlFile(project: Project) = this.isHtml() && project.jcrRoots.contains(this)
 
     private fun VirtualFile.isHtml() = fileType === HtmlFileType.INSTANCE
-
-    private fun VirtualFile.isInJcrRootDirectory(project: Project) = project.jcrRoots.contains(this)
-
 
 }
